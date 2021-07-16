d5ac11c86a0c667e9f6da5579f9d7038549a237a
Negi97Mohit <mohit.snegi97@gmail.com>
2021-07-16 14:56:41 -0400

    test-1

--- Audio.py
+++ Audio.py
@@ -8,9 +8,9 @@ Author: Mohit Negi
 
 import sounddevice as sd
 from scipy.io.wavfile import write
-import wave
 import numpy as np
 import matplotlib.pyplot as plt
+from scipy.fft import fft,ifft
 
 
 fs=44100 #sample rate
@@ -18,10 +18,17 @@ seconds=10 # Duration of recording
 
 myrecording=sd.rec(int(seconds*fs),samplerate=fs,channels=2)
 sd.wait()  #wait until reording is finished
-print(myrecording)
-write('output.wav',fs,myrecording)
+print(myrecording.shape)
+y=fft(myrecording)
+print(y)
 plt.figure(1)
+plt.plot(y)
+write('output.wav',fs,myrecording)
+plt.figure(2)
 plt.title("Signal Wave")
 plt.plot(myrecording)
 plt.show()
+
+
+
 
