8ab403d6510f4cfc2a074ac786dc3524ac74b27f
Negi97Mohit <mohit.snegi97@gmail.com>
2021-07-16 11:29:51 -0400

    Added the Code
    Added the .py code to record audio , store it in myrecording variable as an array. Plot the myrecording graph.

--- Audio.py
+++ Audio.py
@@ -6,3 +6,29 @@ Author: Mohit Negi
 
 """
 
+import sounddevice as sd
+from scipy.io.wavfile import write
+import wave
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+fs=44100 #sample rate
+seconds=10 # Duration of recording
+
+myrecording=sd.rec(int(seconds*fs),samplerate=fs,channels=2)
+sd.wait()  #wait until reording is finished
+print(myrecording)
+write('output.wav',fs,myrecording)
+
+"""record=wave.open("output.wav","r")
+
+#extract raw audio
+signal=record.readframe(-1)
+signal=np.fromstring(signal,"Int16")"""
+
+plt.figure(1)
+plt.title("Signal Wave")
+plt.plot(myrecording)
+plt.show()
+
